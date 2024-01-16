a36c041eea37419738154f919af80cdbbb23007d
Marjorie Taboada Oliveira <122323504+MarjorieTaboada@users.noreply.github.com>
2024-01-16 15:54:20 -0300

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,18 @@
+# Reading the data
+sum_PM25 <- readRDS("/Exploratory Data Analysis Project 2/summarySCC_PM25.rds")
+classif_Code <- readRDS("/Exploratory Data Analysis Project 2/Source_Classification_Code.rds")
+
+#  Total emissions from PM2.5 in the Baltimore City, Maryland from 1999 to 2008
+# Assuming your data has columns named "Emissions", "Year", and "fips"
+# Filter the data based on fips == "24510"
+baltimore <- sum_PM25[sum_PM25$fips == "24510", ]
+
+# Calculate the sum of emissions per year for the filtered data
+baltimore <- aggregate(baltimore$Emissions, by = list(Year = baltimore$year), FUN = sum)
+
+png("plot2.png",width=480,height=480,units="px",bg="transparent")
+
+# Create a bar plot
+barplot(baltimore$x, names.arg = baltimore$Year, xlab = "Year", ylab = "Emissions (Tons)", main = "Total Emissions in Baltimore per Year")
+
+dev.off()
